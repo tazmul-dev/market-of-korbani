85d33177f903ad00fd40e30e00391b3009725b9b
tazmul-dev <mdimrahossan146@gmail.com>
2026-05-02 16:56:55 +0600

    complited animal details page

--- src/app/animalDetails/[id]/page.jsx
+++ src/app/animalDetails/[id]/page.jsx
@@ -1,10 +1,44 @@
+import Smodal from '@/component/Smodal';
+import { animals } from '@/lib/data';
+import { Modal } from '@heroui/react';
+import Image from 'next/image';
 import React from 'react';
 
-const animalDetailsPage = () => {
+const animalDetailsPage = async ({ params }) => {
+    const { id } = await params
+    const animalsData = await animals()
+    const animal = animalsData.find(a => a.id == id)
+    console.log(animal)
+    const { image, name, breed, price, weight, age, location, description } = animal
+
     return (
-        <div>
-            animals Details page
+        <div className=' container mx-auto my-10'>
+            <div className="card lg:card-side bg-base-100 max-w-250 mx-auto shadow-sm">
+                <figure>
+                    <Image
+                        src={image}
+                        alt='name'
+                        width={300}
+                        height={300}
+                        className='w-full h-full object-cover'
+                    />
+                </figure>
+                <div className="card-body">
+                <p>Location {location}</p>
+                  <p>name: {name}</p>
+                  <p>Price:{price}</p>
+                  <p>Weight:{weight}</p>
+                  <p>Breed: {breed}</p>
+                  <p>Age: {age}</p>
+
+                    <p>{description}</p>
+                    <div className="card-actions justify-end">
+                       <Smodal></Smodal>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
